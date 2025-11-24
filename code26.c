784ae1a06d2efc18205537db8be591eba553b576
vermapavani777-ui <vermapavani777@gmail.com>
2025-11-25 01:05:18 +0530

    Create code26.c

---
+++ code26.c
@@ -0,0 +1,11 @@
+// Write a program to print numbers from 1 to n.
+#include <stdio.h>
+int main()
+{ 
+    int i,n;
+    printf("enter the value of n: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    printf("%d",i);
+ return 0;
+}
