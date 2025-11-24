467b12ce6b23ca11c63248ae858badb49e8da2a0
vermapavani777-ui <vermapavani777@gmail.com>
2025-11-25 01:06:12 +0530

    Create code27.c

---
+++ code27.c
@@ -0,0 +1,12 @@
+// Write a program to print the sum of the first n odd numbers.
+#include <stdio.h>
+int main()
+{ 
+    int num;
+    int sum;
+    printf("enter the odd number: ");
+    scanf("%d",&num);
+     sum=num*num;
+     printf("sum is=%d",sum);
+ return 0;
+}
