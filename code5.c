f19cb816c50811280cb2ac7738d3d5cab4a85218
vermapavani777-ui <vermapavani777@gmail.com>
2025-11-25 00:28:22 +0530

    Create code5.c

---
+++ code5.c
@@ -0,0 +1,11 @@
+// write a program to convert temperature from celcius to fahrenheit.
+#include <stdio.h>
+int main() 
+{
+    int c;
+    printf("enter the celcius: ");
+    scanf("%d",&c);
+int fahrenheit=(c*9/5)+32;
+printf("fahrenheit is: %d\n",fahrenheit);
+ return 0;
+}
