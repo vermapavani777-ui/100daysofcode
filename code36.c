d0e40739e5ed81327a2881ce444786fe6641d166
vermapavani777-ui <vermapavani777@gmail.com>
2025-11-25 13:00:37 +0530

    Create code36.c

---
+++ code36.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Function to find GCD using Euclidean algorithm
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int main() {
+    int num1, num2;
+
+    // Input two numbers
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    // Calculate and display the GCD
+    printf("GCD of %d and %d is %d\n", num1, num2, gcd(num1, num2));
+
+    return 0;
+}
