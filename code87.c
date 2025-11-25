fac2c624c28a43e17b99e1fb00cb9fb5871f8563
vermapavani777-ui <vermapavani777@gmail.com>
2025-11-25 15:05:17 +0530

    Add string analysis program to count spaces, digits, and special characters

---
+++ code87.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int spaces = 0, digits = 0, specialChars = 0, i = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[i] != '\0' && str[i] != '\n') {
+        if (str[i] == ' ') {
+            spaces++;
+        } else if (str[i] >= '0' && str[i] <= '9') {
+            digits++;
+        } else if (!((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))) {
+            specialChars++;
+        }
+        i++;
+    }
+
+    printf("Spaces: %d\n", spaces);
+    printf("Digits: %d\n", digits);
+    printf("Special characters: %d\n", specialChars);
+
+    return 0;
+}
