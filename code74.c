2900cc8feb3542cf0d713b4cf6d8e5cc2ccd3347
vermapavani777-ui <vermapavani777@gmail.com>
2025-11-25 14:50:01 +0530

    Create code74.c

---
+++ code74.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    printf("Enter number of rows: ");
+    scanf("%d", &m);
+    printf("Enter number of columns: ");
+    scanf("%d", &n);
+
+    int matrix[m][n], transpose[n][m];
+
+    printf("Enter elements of the matrix:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    // Computing transpose
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            transpose[j][i] = matrix[i][j];
+        }
+    }
+
+    printf("Transpose of the matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d\t", transpose[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
